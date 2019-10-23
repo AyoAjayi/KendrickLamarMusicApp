25bb87bcb925065520f6f4ea59cc2d7d5f7e5765
Ubuntu <ubuntu@ip-172-31-23-218.ec2.internal>
2019-10-23 14:02:53 +0000

    Added my Kendrick Lamar Music App

---
+++ README.md
@@ -0,0 +1,10 @@
+# 1) The theme I chose for this project is Kendrick Lamar. This web app gave me an opportunity to let users see Kendrick Lamar's songs, view what people are saying about him, and actually listen to short snippets of his music.
+
+# 2) For the genius api, I queried kendrick Lamar in the api url to search for his songs, images, and link to his genius page. I did the same for the twitter api. I specifically queried Kendrick Lamar's name to generate random tweets about him.
+
+# 3) I struggled with parsing the json data intially. I solved this by looking at genius to see the way the rrsponse would be formatted.
+# I also struggled with the idea of making my webpage dynamic. Once I understood the structure of the json data, then I saw a way to use random to randomly get the information I needed.
+# It took me a while to get my config vars set up, I reviewed the slides for more clariification.
+
+# 4) I think my css code could be more concise. I referenced w3schools so a lot of the design was trial and error for me.
+# 5) I wish I could find a way to block out the profanity in the tweets in order to improve my project.
---
+++ myapp.py
@@ -0,0 +1,76 @@
+import flask
+import os
+import requests
+import json
+import random
+import requests_oauthlib, requests
+
+
+app = flask.Flask(__name__)
+    
+@app.route('/') 
+def artist_info(): 
+    url = "https://api.genius.com/search?q=Kendrick%20Lamar"
+    genius_key = os.getenv("GeniusKey")
+    my_headers = {
+        "Authorization": "Bearer  " + genius_key
+    }
+    response = requests.get(url, headers=my_headers)
+    json_body = response.json() 
+    
+    array = json_body["response"]["hits"]
+    r = random.randint(0, len(array) - 1) 
+    
+    #This gets the id of the song that's playing
+    song_id = array[r]["result"]["id"]
+    song_link = get_songPlaying(song_id)
+  
+    
+    image_url = array[r]["result"]["song_art_image_url"]
+    return flask.render_template("index.html", artist = array[r]["result"]["primary_artist"]["name"], song = array[r]["result"]["title"], photo = image_url, tweet = twitter_quotes(), video = song_link)
+   
+   
+def twitter_quotes():
+    twitter_url = "https://api.twitter.com/1.1/search/tweets.json?q=KendrickLamar&count=10"
+    twitter_key = os.getenv("TwitterKey")
+    twitter_secret_key = os.getenv("SecretKey")
+    twitter_token = os.getenv("Token")
+    twitter_secret_token = os.getenv("SecretToken")
+    
+    
+    oauth = requests_oauthlib.OAuth1(
+        twitter_key,
+        twitter_secret_key,
+        twitter_token,
+        twitter_secret_token
+     )
+    
+    response = requests.get(twitter_url, auth=oauth)
+    json_body = response.json()
+    array = json_body['statuses']
+    
+    num = random.randint(0, len(array) - 1)
+    different_tweets =  array[num]["text"]
+    return different_tweets
+   
+    
+def get_songPlaying(song_id):
+    song_id_url= "https://api.genius.com/songs/" + str(song_id)
+    song_id_genius_key = "sZD99vUmHfcsYnAwtmTIBWsDgTJZjO8qAdzKk5JgGxeMdM4gsktvql3il_0kw0-D"
+    song_id_headers = {
+            "Authorization": "Bearer  " + song_id_genius_key
+    }
+    response2 = requests.get(song_id_url, headers=song_id_headers)
+    json_body2 = response2.json()
+    
+   
+    song_link = json_body2["response"]["song"]["apple_music_player_url"]
+    return song_link
+
+
+app.run(
+    port=int(os.getenv('PORT', 8080)),
+    host=os.getenv('IP', '0.0.0.0'),
+    debug=True
+)
+
